d4797ee8308e94e4fcc5361fbc658b846c777816
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2023-11-02 18:52:16 +0100

    feat: revert changes related to csrf and apply helmet

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as csurf from 'csurf';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,8 +10,27 @@ async function bootstrap() {
   // Pipe activation for DTO validation
   app.useGlobalPipes(new ValidationPipe());
 
-  // Enable the APP CSRF middleware
-  app.use(csurf());
+  // Protect the APP from well known web vulnarabilities middleware
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
 
   // Host is neede in case of cloud server deployment
   await app.listen(3000, '0.0.0.0');
